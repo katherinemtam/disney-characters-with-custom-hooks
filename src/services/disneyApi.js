415cd9c43198123f307fc61227ba13c174b68e93
Katherine Tam <katherinemtam@gmail.com>
2021-08-05 00:18:19 -0700

    fetchCharacter function

--- src/services/disneyApi.js
+++ src/services/disneyApi.js
@@ -5,3 +5,12 @@ export const fetchCharacters = async () => {
 
   return data;
 };
+
+export const fetchCharacter = async (id) => {
+  console.log('api', id);
+  const res = await fetch(`https://api.disneyapi.dev/characters/${id}`);
+  const json = await res.json();
+  console.log('api', json);
+
+  return json;
+};
