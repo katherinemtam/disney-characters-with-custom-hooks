f4dbc9e46f7c7c9db6d246c0a4cf66857b833dfc
Katherine Tam <katherinemtam@gmail.com>
2021-08-05 10:36:01 -0700

    mock testing for both app tests, tests pass

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
 import { MemoryRouter } from 'react-router-dom';
 import App from './App';
+import testData from '../../../test.json';
+
+const server = setupServer(
+  rest.get('https://api.disneyapi.dev/characters?page=5', (req, res, ctx) => {
+    return res(ctx.json(testData));
+  })
+);
+
+const server2 = setupServer(
+  rest.get('https://api.disneyapi.dev/characters/308', (req, res, ctx) => {
+    return res(
+      ctx.json({
+        _id: 308,
+        films: ['Tangled', 'Tangled: Before Ever After'],
+        shortFilms: ['Tangled Ever After', 'Hare Peace'],
+        tvShows: ['Once Upon a Time', 'Tangled: The Series'],
+        videoGames: [
+          'Disney Princess Enchanting Storybooks',
+          'Hidden Worlds',
+          'Disney Crossy Road',
+          'Kingdom Hearts III',
+        ],
+        parkAttractions: ['Celebrate the Magic', 'Jingle Bell, Jingle BAM!'],
+        allies: [],
+        enemies: [],
+        sourceUrl: 'https://disney.fandom.com/wiki/Queen_Arianna',
+        name: 'Queen Arianna',
+        imageUrl:
+          'https://static.wikia.nocookie.net/disney/images/1/15/Arianna_Tangled.jpg/revision/latest?cb=20160715191802',
+        createdAt: '2021-04-12T01:33:34.458Z',
+        updatedAt: '2021-04-12T01:33:34.458Z',
+        url: 'https://api.disneyapi.dev/characters/308',
+        __v: 0,
+      })
+    );
+  })
+);
 
 describe('App component', () => {
+  beforeAll(() => server.listen());
+  afterAll(() => server.close());
+
   it('renders a list of characters on the home page', async () => {
     render(
       <MemoryRouter>
@@ -16,8 +58,12 @@ describe('App component', () => {
     const ul = await screen.findByRole('list');
     expect(ul).not.toBeEmptyDOMElement();
   });
+});
 
-  it.only('renders a characters detail page', async () => {
+describe('App component', () => {
+  beforeAll(() => server2.listen());
+  afterAll(() => server2.close());
+  it('renders a characters detail page', async () => {
     render(
       <MemoryRouter initialEntries={['/308']}>
         <App />
