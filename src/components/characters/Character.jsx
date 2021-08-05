aa40e57bc83082fc00fda274a35fc8ab915556f0
Katherine Tam <katherinemtam@gmail.com>
2021-08-05 00:17:25 -0700

    change list item info

--- src/components/characters/Character.jsx
+++ src/components/characters/Character.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 
-const Character = ({ id, name, imageUrl }) => {
+const Character = ({ name, films, tvShows }) => {
+  const stringyFilms = JSON.stringify(films);
+  const stringyTvShows = JSON.stringify(tvShows);
+
   return (
-    <Link to={id}>
+    <>
       <h2>{name}</h2>
-      <img src={imageUrl} alt={name} />
-    </Link>
+      <p>Films: {stringyFilms}</p>
+      <p>TV Shows: {stringyTvShows}</p>
+    </>
   );
 };
 
 Character.propTypes = {
-  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string.isRequired,
+  films: PropTypes.arrayOf(PropTypes.string).isRequired,
+  tvShows: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
 
 export default Character;
