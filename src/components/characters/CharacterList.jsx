94fda5f5622130fa6be89ef60f79dfad2aae8487
Katherine Tam <katherinemtam@gmail.com>
2021-08-05 00:17:39 -0700

    add link to list items

--- src/components/characters/CharacterList.jsx
+++ src/components/characters/CharacterList.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { useCharacters } from '../../state/character';
+import { Link } from 'react-router-dom';
 import Character from './Character';
 
 const CharacterList = () => {
   const characters = useCharacters();
 
   const characterElements = characters.map((character) => (
-    <li key={character.id}>
-      <Character {...character} />
+    <li key={character._id}>
+      <Link to={`/${character._id}`}>
+        <Character {...character} />
+      </Link>
     </li>
   ));
 
